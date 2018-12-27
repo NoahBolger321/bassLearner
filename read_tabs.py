4ce79eb7f7ab650e71ff1a04a6ed274746b8aeb7
NoahBolger321 <ncbolger@uwaterloo.ca>
2018-12-27 15:21:44 -0500

    store notes in array, reduce code

--- read_tabs.py
+++ read_tabs.py
@@ -6,25 +6,20 @@ root = os.path.dirname(os.path.abspath(__file__))
 path = "{}/tablature/bitesDust.txt".format(root)
 with open(path, 'r') as tabText:
     tabLines = tabText.readlines()
-
-# TODO: read notes into list in order for nearest neighbour algorithm
 tabLines = [t.strip() for t in tabLines if t.strip() != '']
-# loop through each grouping of strings, retrieve notes, print mapped note if fret recorded
-for n in range(0, len(tabLines), 4):
-    for i in range(1, len(tabLines[n])):
-        gNote = tabLines[n][i]
-        dNote = tabLines[n+1][i]
-        aNote = tabLines[n+2][i]
-        eNote = tabLines[n+3][i]
-        if gNote.isdigit():
-            string = tabLines[n][0]
-            print(stringMap.map[gNote + string])
-        elif dNote.isdigit():
-            string = tabLines[n+1][0]
-            print(stringMap.map[dNote + string])
-        elif aNote.isdigit():
-            string = tabLines[n+2][0]
-            print(stringMap.map[aNote + string])
-        elif eNote.isdigit():
-            string = tabLines[n+3][0]
-            print(stringMap.map[eNote + string])
+
+
+# loop through each grouping of strings, retrieve notes, print mapped note if fret recorded (dynamic # of strings)
+def get_notes(notes, num_strings):
+    for n in range(0, len(tabLines), num_strings):
+        for i in range(1, len(tabLines[n])):
+            for j in range(0, num_strings):
+                note = tabLines[n+j][i]
+                string = tabLines[n+j][0]
+                if note.isdigit():
+                    notes.append(stringMap.map[note + string])
+    return notes
+
+
+notesArr = []
+print(get_notes(notesArr, 4))
